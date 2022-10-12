0502bd3bb1dde4b4e1b9bcdbce7609e3c3e7af02
Jacob Sanford <jsanford@unb.ca>
2022-10-12 08:19:39 -0300

    TESTING-1 Migrate testing from cypress repo

---
+++ tests/cypress/status/e2e/womenactivists-lib-unb-ca.cy.js
@@ -0,0 +1,36 @@
+const host = 'https://womenactivists.lib.unb.ca'
+describe('Woman Social Activists of Atlantic Canada', {baseUrl: host, groups: ['sites']}, () => {
+
+  context('Front page', {baseUrl: host}, () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.title()
+        .should('contain', 'Women Social Activists of Atlantic Canada')
+    })
+
+    specify('Menu should contain link to "Activists"', () => {
+      cy.get('#navbar')
+        .contains(/^Activists$/)
+        .its('0.href')
+        .should('match', /\/activists$/)
+    });
+  })
+
+  context('Activists page', {baseUrl: `${host}/activists?tid=1`}, () => {
+    beforeEach(() => {
+      cy.visit('')
+      cy.title()
+        .should('contain', 'Activists')
+    })
+
+    specify('9+ activists\' profiles should be listed, including "Mary Majka"', () => {
+      cy.get('.view-activists .views-row').as('activists')
+        .should('have.lengthOf.at.least', 9)
+      cy.get('@activists')
+        .contains('Mary Majka')
+        .its('0.href')
+        .should('match', /\/content\/mary-majka$/)
+    });
+  })
+
+})
